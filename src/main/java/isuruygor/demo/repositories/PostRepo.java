d66af2574e7c5f0eb162f31a00b70205f80a7b2c
Ygor Garofalo <igorgarofalo@gmail.com>
2024-03-16 21:08:03 +0100

    Adds method for retrieving posts by category

--- src/main/java/isuruygor/demo/repositories/PostRepo.java
+++ src/main/java/isuruygor/demo/repositories/PostRepo.java
@@ -2,8 +2,16 @@ package isuruygor.demo.repositories;
 
 import isuruygor.demo.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepo extends JpaRepository<Post, Long> {
+
+
+    @Query(name = "SELECT p FROM Post p WHERE p.category= :category")
+    List<Post> getPostsByCategory(@Param("category") String category);
 }
